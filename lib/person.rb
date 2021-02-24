306bfe9e42d505e289ff91ccf5223a63ca55507b
Bryecook <brybcook@gmail.com>
2021-02-24 12:42:01 -0800

    Brian's Section

--- lib/person.rb
+++ lib/person.rb
@@ -31,6 +31,40 @@ class Person
         "all about the benjamins"
     end
 
+    def work_out
+        @happiness= @happiness + 2
+        @hygiene= @hygiene - 3
+        self.hygiene=(@hygiene)
+        self.happiness=(@happiness)
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        @happiness=@happiness + 3
+        friend.happiness= friend.happiness + 3 
+        self.happiness=(@happiness)
+        "Hi #{friend.name}! It's #{@name}. How are you?"
+    end 
+
+    def start_conversation(person, topic)
+        if topic=="politics"
+            @happiness=@happiness-2
+            self.happiness=(@happiness)
+            person.happiness= person.happiness - 2
+            "blah blah partisan blah lobbyist"
+
+        elsif topic=="weather"
+            @happiness=@happiness+1
+            self.happiness=(@happiness)
+            person.happiness= person.happiness + 1 
+            "blah blah sun blah rain"
+
+        else
+            "blah blah blah blah blah"
+        end
+
+    end
+
     def hygiene=(hygiene)
         if hygiene > 10
             @hygiene = 10
